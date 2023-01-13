cf4ad8a77ab05eb7332415fbd2db5e793bca0903
Brian Chapman <chapman.brian86@gmail.com>
2023-01-13 17:38:43 -0500

    Created the Reaction subdocument

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    max: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    get: (date) => {
+      if (date) return date.toISOString().split("T")[0];
+    },
+  },
+});
+
+module.exports = reactionSchema;
