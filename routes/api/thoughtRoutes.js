d42eea437a4ec1dcbac559d5905ba34c5281ee78
Brian Chapman <chapman.brian86@gmail.com>
2023-01-15 11:31:01 -0500

    Laid out all routes and not writing logic for User functions to be exported

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('');
+
+// localhost:3001/api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// localhost:3001/api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// localhost:3001/api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction);
+
+// localhost:3001/api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('');
+
+// localhost:3001/api/users
+router.route('/').get(getUsers).post(createUser);
+
+// localhost:3001/api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// localhost:3001/api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
