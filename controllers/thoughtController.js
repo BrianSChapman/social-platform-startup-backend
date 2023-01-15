268f5501f0a999340b62d389f295d5ce1b000474
Brian Chapman <chapman.brian86@gmail.com>
2023-01-15 18:22:43 -0500

    Working out Thought and Reaction API logic

---
+++ controllers/thoughtController.js
@@ -0,0 +1,71 @@
+const Thought = require("../models/Thought");
+const User = require("../models/user");
+// const Reaction = require('../models/Reaction');
+const { ObjectId } = require("mongoose").Types;
+
+module.exports = {
+  // Get all Thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Get a single Thought by ID
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "Unable to find this thought." })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  //   create a new Thought and add it to user's existing Thought's array field.
+  createThought(req, res) {
+    Thought.create(req.body).then((thought) => res.json(thought));
+    User.findOneAndUpdate(
+      { _id: req.params.thoughts },
+      { $addToSet: { thoughts: req.body } },
+      { runValidators: true, new: true }
+    ).catch((err) => {
+      console.log(err);
+      return res.status(500).json(err);
+    });
+  },
+
+  //  Update an existing Thought, targeting by ID
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'Unable to find this thought.' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+//   Delete an existing Thought, targeting by ID
+deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+    .then((thought) => 
+    !thought ? res.status(404).json({ message: 'Unable to find this thought.'})
+    : res.json({ message: 'Your thought has been deleted' }))
+    .catch((err) => res.status(500).json(err));
+    },
+
+// Create a reaction to be stored in a Thought's 'reactions' array field
+createReaction(req, res) {
+    Thought.findOneAndUpdate(
+        { _id: req.params.id},
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+    )
+}
+};
--- models/Thought.js
+++ models/Thought.js
@@ -1,34 +1,31 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now(),
-            get: (date) => {
-                if (date) return date.toISOString().split('T')[0];
-            }
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reaction: [{
-            type: Schema.Types.ObjectId, ref: 'reactions'
-        }],
-    }
-    );
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    get: (date) => {
+      if (date) return date.toISOString().split("T")[0];
+    },
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+  // [{ type: Schema.Types.ObjectId, ref: 'reactions'}],
+});
 
-    thoughtSchema.virtual('reactionCount').get(function () { 
-        return this.reactions.length;
-    });
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
-    const Thought = model('Thought', thoughtSchema);
+const Thought = model("Thought", thoughtSchema);
 
-    module.exports = Thought;
+module.exports = Thought;
