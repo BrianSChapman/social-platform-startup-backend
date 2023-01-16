14c46700c07ace763d4827934fce354b8a1c1d25
Brian Chapman <chapman.brian86@gmail.com>
2023-01-16 14:50:28 -0500

    Debugged connection and server is up. Now testing/ debugging routes

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/socialPlatform', {
+const connectionString = 
+process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialPlatform';
+
+connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
